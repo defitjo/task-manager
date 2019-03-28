146b866092a566303c814c685152d0e69845aa7d
defitjo <defitjo16@gmail.com>
2019-03-28 18:31:22 -0400

    created navbar

--- taskmanager/src/App.js
+++ taskmanager/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Menu from './components/design/Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Menu />
       </div>
     );
   }
---
+++ taskmanager/src/components/design/Menu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <nav className="ui blue inverted menu">
+      <a href="" className="active item">
+        Tasker
+      </a>
+      <div className="right menu">
+        <a href="" className="item">
+          Log-in
+        </a>
+        <a href="" className="item">
+          Sign Up
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Menu
+
