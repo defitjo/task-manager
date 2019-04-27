8b2f08a0fc56221e8fd4c03ff122b4f67e0830c3
defitjo <defitjo16@gmail.com>
2019-04-27 02:09:56 -0400

    deployed app to firebase

--- taskmanager/README.md
+++ taskmanager/README.md
@@ -1,1 +1,3 @@
 # React Redux App using Firebase/Firestore and Semantic UI
+
+Link to app: <https://task-manager-d42e0.firebaseapp.com/>
---
+++ taskmanager/dist/precache-manifest.ddb79f253b00f26baf8d18964e6dc193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b0dec19410e3954910d3",
+    "url": "/static/js/main.a46f68bd.chunk.js"
+  },
+  {
+    "revision": "0e88911d66c062c79229",
+    "url": "/static/js/2.a1890c58.chunk.js"
+  },
+  {
+    "revision": "b0dec19410e3954910d3",
+    "url": "/static/css/main.75c7fa86.chunk.css"
+  },
+  {
+    "revision": "887d3d227ea87c25ff522eabfc064661",
+    "url": "/index.html"
+  }
+];
